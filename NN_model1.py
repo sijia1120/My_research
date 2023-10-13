c2bf70fbcccd61467585d65a3751d72375483c25
Scarlett <98492102+sijia1120@users.noreply.github.com>
2023-10-13 23:07:20 +0100

    Update NN_model1.py

--- NN_model1.py
+++ NN_model1.py
@@ -1,11 +1,10 @@
-from google.colab import drive
-drive.mount('/content/drive')
+from keras_tuner import HyperModel
+import keras_tuner as kt
 
-!pip install keras_tuner
 import numpy as np
 import pandas as pd
 #!pip install keras_tuner
-import keras_tuner
+#import keras_tuner
 
 import keras
 from keras import models, layers, metrics, Sequential
@@ -13,13 +12,124 @@ from keras.layers import Dense, BatchNormalization, Dropout
 import tensorflow as tf
 from keras.regularizers import l1,l2,l1_l2
 from sklearn.metrics import mean_squared_error
+from tensorflow.keras.layers import Input, Dense, Multiply, Concatenate
+from tensorflow.keras.models import Model
 
 import tensorflow as tf
 import random
 
-# Set random seed for reproducibility during tuning
+import keras_tuner
 from keras_tuner import HyperModel
 import keras_tuner as kt
+
+% ------------------------
+char_core = ['acc','agr','beta','bm','cash','cashpr','cfp','chatoia','chcsho','chfeps','chinv',
+             'chmom','chpmia','chtx','currat','depr','dy','ear','ep','gma','grcapx','grltnoa',
+             'ill','indmom','invest','lev','lgr','maxret','mom12m','mom1m','mom36m','mve','nincr',
+             'orgcap','pchgm_pchsale','pchsale_pchinvt','pchsale_pchrect','pchsale_pchxsga',
+             'retvol','roaq','roavol','roeq','salecash','saleinv','sgr','sp','std_dolvol','std_turn','turn']
+
+#merged_latent.to_pickle('merged_latent49.pkl')
+merged_latent49 = pd.read_pickle("merged_latent49.pkl")
+
+firm_col = merged_latent49.columns.tolist()[18:18+49]
+latent_col = merged_latent49.columns.tolist()[18+49+1:]
+print(len(firm_col), len(latent_col))
+
+% ------------------------------------------------------------------------
+df_train = merged_latent49[merged_latent49.index < pd.Period((str(1995)+"-1"),freq='M')]
+df_val = merged_latent49[(merged_latent49.index >= pd.Period((str(1995)+"-1"),freq='M')) & (merged_latent49.index < pd.Period((str(2000)+"-1"),freq='M'))]
+df_test = merged_latent49[merged_latent49.index >= pd.Period((str(2000)+"-1"),freq='M')]
+
+merged_train_firm = df_train[firm_col]
+merged_train_latent = df_train[latent_col]
+merged_train_y = df_train['predicted_return']
+print(merged_train_firm.shape, merged_train_latent.shape, merged_train_y.shape)
+
+merged_val_firm = df_val[firm_col]
+merged_val_latent = df_val[latent_col]
+merged_val_y = df_val['predicted_return']
+print(merged_val_firm.shape, merged_val_latent.shape, merged_val_y.shape)
+
+merged_test_firm = df_test[firm_col]
+merged_test_latent = df_test[latent_col]
+merged_test_y = df_test['predicted_return']
+print(merged_test_firm.shape, merged_test_latent.shape, merged_test_y.shape)
+
+% -------------------------------------------------------------------------
+def my_metric_fn(y_true, y_pred):
+    num = tf.reduce_mean(tf.square(y_true - y_pred))
+    den = tf.reduce_mean(tf.square(y_true))
+    return 1 - num / den
+
+def multi_input_model(num_input, dropout_, l1_reg_, l2_reg_):
+    dropout = dropout_
+    l1_reg = l1_reg_
+    l2_reg =l2_reg_
+    input1 = Input(shape=(num_input,))
+    input2 = Input(shape=(num_input,))
+    x1 = Dense(16, activation='relu')(input2)
+    x1 = Dense(4, activation='relu')(x1)
+    x1 = Dense(16, activation='relu')(x1)
+    x1 = Dense(num_input, activation='relu')(x1)
+
+    merged = Multiply()([input1, x1])
+    x2 = Dense(32, activation='relu')(merged)
+    x2 = Dense(16, activation='relu')(x2)
+    #x2 = Dense(8, activation='relu')(x2)
+    output = Dense(1, activation='linear')(x2)
+    model = Model(inputs=[input1, input2], outputs=output)
+    optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=0.0001, beta_1=0.92) 
+    #model.compile(optimizer=optimizer, loss='mean_absolute_error', metrics=[my_metric_fn])
+    model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=[my_metric_fn])
+    return model 
+
+def Autoencoder1(num_input,lr=0.0001, acti='relu'):
+    input_img = Input(shape=(num_input, ))
+    encoded1 = Dense(16, activation=acti)(input_img)
+    encoded2 = Dense(4, activation=acti)(encoded1)
+    decoded2 = Dense(16, activation=acti)(encoded2)
+    decoded1 = Dense(num_input, activation=acti)(decoded2)
+    
+    autoencoder = Model(inputs=input_img, outputs=decoded1)
+    opt = tf.keras.optimizers.legacy.Adam(learning_rate=lr)
+    autoencoder.compile(loss='mean_absolute_error',optimizer=opt, metrics=[my_metric_fn])
+   #autoencoder.compile(loss='mean_squared_error', optimizer=opt, metrics=[my_metric_fn])
+    return autoencoder
+
+% -----------------------------------------------------------
+## Conditional Autoencoder
+import random 
+seed_list = [458, 165, 530, 564, 590, 560, 829, 170, 376, 176]
+yhat_df = pd.DataFrame()
+
+for random_seed in seed_list:
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    tf.random.set_seed(random_seed)
+    autoencoder = Autoencoder1(num_input=merged_train_latent.shape[1])
+    autoencoder.fit(merged_train_latent, merged_train_latent, 
+                epochs = 20, batch_size = 216,
+                validation_data = (merged_val_latent, merged_val_latent),
+                 callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss',mode='min', patience=1)])
+ 
+    model = multi_input_model(num_input=merged_train_latent.shape[1], 
+                              dropout_=0.2, 
+                              l1_reg_=0.01, 
+                              l2_reg_=0.01,)
+    model.layers[0].set_weights(autoencoder.layers[0].get_weights())
+    model.layers[2].set_weights(autoencoder.layers[2].get_weights())
+    model.layers[0].trainable = False
+    model.layers[2].trainable = False
+    model.fit([merged_train_latent, merged_train_firm], merged_train_y,
+                   epochs=50,
+                   validation_data=([merged_val_latent, merged_val_firm], merged_val_y),
+                   callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss',mode='min', patience=1)])
+    print(model.evaluate([merged_test_latent, merged_test_firm], merged_test_y))
+    y_hat = model.predict([merged_test_latent, merged_test_firm]).reshape(-1)
+    yhat_df[random_seed] = y_hat
+    print()
+
 
 
 class DataPrepare():
