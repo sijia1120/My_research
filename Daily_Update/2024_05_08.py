378137fd6bfbaa4cc36ec436017c0bdb37271c3d
Scarlett <98492102+sijia1120@users.noreply.github.com>
2024-05-08 11:42:51 +0100

    Create 2024_05_08.py

---
+++ Daily_Update/2024_05_08.py
@@ -0,0 +1,129 @@
+###  -------  From Theory to Practice with Bayesian Neural Network, Using Python --------
+### https://towardsdatascience.com/from-theory-to-practice-with-bayesian-neural-network-using-python-9262b611b825
+
+
+pip install torchbnn
+
+model = nn.Sequential(
+    bnn.BayesLinear(prior_mu=0, prior_sigma=0.1, in_features=1, out_features=1000),
+    nn.ReLU(),
+    bnn.BayesLinear(prior_mu=0, prior_sigma=0.1, in_features=1000, out_features=1),
+)
+
+
+# Actually, there is a library to convert your torch model into its Bayesian surrogate:
+transform_model(model, nn.Conv2d, bnn.BayesConv2d, 
+                args={"prior_mu":0, "prior_sigma":0.1, "in_channels" : ".in_channels",
+                      "out_channels" : ".out_channels", "kernel_size" : ".kernel_size",
+                      "stride" : ".stride", "padding" : ".padding", "bias":".bias"
+                     }, 
+                attrs={"weight_mu" : ".weight"})
+
+
+# -------  4. Hands-On regression task --------------------------
+import numpy as np
+from sklearn import datasets
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchbnn as bnn
+import matplotlib.pyplot as plt
+
+
+
+# After that, we will make our very simple bidimensional dataset:
+def clean_target(x):
+    return x.pow(5) -10* x.pow(1)+1
+def target(x):
+    return x.pow(5) -10* x.pow(1) + 2*torch.rand(x.size())
+  
+x = torch.linspace(-2, 2, 500)
+y = x.pow(5) -10* x.pow(1) + 2*torch.rand(x.size())
+x = torch.unsqueeze(x, dim=1)
+y = torch.unsqueeze(y, dim=1)
+
+plt.scatter(x.data.numpy(), y.data.numpy())
+plt.show()
+
+
+# Now we will define our Bayesian feed-forward neural network:
+model = nn.Sequential(
+    bnn.BayesLinear(prior_mu=0, prior_sigma=0.1, in_features=1, out_features=1000),
+    nn.ReLU(),
+    bnn.BayesLinear(prior_mu=0, prior_sigma=0.1, in_features=1000, out_features=1),)
+
+mse_loss = nn.MSELoss()
+kl_loss = bnn.BKLLoss(reduction='mean', last_layer_only=False)
+kl_weight = 0.01
+
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+# Both the losses will be used in our optimization step:
+for step in range(2000):
+    pre = model(x)
+    mse = mse_loss(pre, y)
+    kl = kl_loss(model)
+    cost = mse + kl_weight*kl
+    
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+    
+print('- MSE : %2.2f, KL : %2.2f' % (mse.item(), kl.item()))
+
+# Let’s define our test set: 
+x_test = torch.linspace(-2, 2, 300)
+y_test = target(x_test)
+
+x_test = torch.unsqueeze(x_test, dim=1)
+y_test = torch.unsqueeze(y_test, dim=1)
+models_result = np.array([model(x_test).data.numpy() for k in range(10000)])
+models_result = models_result[:,:,0]    
+models_result = models_result.T
+mean_values = np.array([models_result[i].mean() for i in range(len(models_result))])
+std_values = np.array([models_result[i].std() for i in range(len(models_result))])
+
+# and we will plot our confidence intervals.
+plt.figure(figsize=(10,8))
+plt.plot(x_test.data.numpy(),mean_values,color='navy',lw=3,label='Predicted Mean Model')
+plt.fill_between(x_test.data.numpy().T[0],mean_values-3.0*std_values,mean_values+3.0*std_values,alpha=0.2,color='navy',label='99.7% confidence interval')
+#plt.plot(x_test.data.numpy(),mean_values,color='darkorange')
+plt.plot(x_test.data.numpy(),y_test.data.numpy(),'.',color='darkorange',markersize=4,label='Test set')
+plt.plot(x_test.data.numpy(),clean_target(x_test).data.numpy(),color='green',markersize=4,label='Target function')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+
+
+# ------------------------------ Synthetic Dataset for input 49 variable output 1 variable ------------
+# Generate random data
+num_samples = 1000
+num_features = 49
+X = np.random.rand(num_samples, num_features)  # Features (49 dimensions)
+y = np.random.rand(num_samples)               # Target variable (1 dimension)
+
+# Print shapes to confirm dimensions
+print("X shape:", X.shape)
+print("y shape:", y.shape)
+
+# Convert NumPy arrays to PyTorch tensors
+X = torch.tensor(X, dtype=torch.float32)
+y = torch.tensor(y, dtype=torch.float32)
+
+# Unsqueeze to make y 2D tensor with shape (num_samples, 1)
+y = torch.unsqueeze(y, dim=1)
+
+# Print shapes to confirm dimensions
+print("X shape:", X.shape)
+print("y shape:", y.shape)
+
+
+# Now we will define our Bayesian feed-forward neural network:
+model = nn.Sequential(
+    bnn.BayesLinear(prior_mu=0, prior_sigma=0.1, in_features=num_features, out_features=1000),
+    nn.ReLU(),
+    bnn.BayesLinear(prior_mu=0, prior_sigma=0.1, in_features=1000, out_features=1),)
+
+
+
+
